ab12e60362b3d685806693f6c8527fa8d6c50a68
Joseph Tran <100446142+jknt27@users.noreply.github.com>
2024-03-09 02:38:16 -0800

    Remove FC

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -7,7 +7,7 @@ export default function Menu () {
             <div className="dropdown">
                 <li><img src={require('./img/favicon.png')} alt='A picture of the website logo.' /></li>
                 <li><NavLink to='Age-Calculator/tntt' className='link'><span className='material-symbols-outlined'>TNTT</span></NavLink></li>
-                <li><NavLink to='Age-Calculator/fc' className='link'>FC</NavLink></li>
+                {/* <li><NavLink to='Age-Calculator/fc' className='link'>FC</NavLink></li> */}
             </div>
         </nav>
     )
